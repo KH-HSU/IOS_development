75a1da3100ad017c087ba550a495b13f120125df
KH-Hsu <nlpss92075@gmail.com>
2020-12-11 02:01:20 +0800

    Hello swift

---
+++ Playground/Hello_Xcode_swift.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+// use var before using variables
+
+var str:String = "Hello, playground"
+print(str)
+
+var a = 1
+var b = 1
+print(a+b)
+
+// constant
+let c = 1
+
+// data type
+var d:Float = 2.3
+var e:Double = 13.9
+var f:Bool = true
+
+// statement
+if a == 1{
+    print("only if a is equal to one")
+}
