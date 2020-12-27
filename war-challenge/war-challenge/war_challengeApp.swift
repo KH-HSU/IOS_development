cfcd21ef0d5f24718a924c9ba3df2b476ca8b4a8
KH-Hsu <nlpss92075@gmail.com>
2020-12-27 15:29:29 +0800

    Lesson 5

---
+++ war-challenge/war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by 許顥屏 on 2020/12/18.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
